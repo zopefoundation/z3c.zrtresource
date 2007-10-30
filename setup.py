04d1ba5144df62898da35c868d0b6f8806577810
Stephan Richter <stephan.richter@gmail.com>
2007-10-30 21:24:07 +0000

    Get ready for first public release.

--- setup.py
+++ setup.py
@@ -1,14 +1,59 @@
-#!python
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for z3c.zrtresource package
+
+$Id$
+"""
+import os
 from setuptools import setup, find_packages
 
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
 setup(name='z3c.zrtresource',
-      version='0.1.1',
-      author = "Zope Community",
-      author_email = "zope3-dev@zope.org",
-      description = open("README.txt").read(),
-      license = "ZPL 2.1",
-      keywords = "css resource zope zope3",
-      url='http://svn.zope.org/z3c.zrtresource',
+      version = '1.0.0',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      description='Zope Resource Templates',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          'Detailed Dcoumentation\n' +
+          '======================\n'
+          + '\n\n' +
+          read('src', 'z3c', 'zrtresource', 'README.txt')
+          + '\n\n' +
+          read('src', 'z3c', 'zrtresource', 'zcml.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 css javascript resource zope",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/z3c.zrtresource',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['z3c'],
       extras_require = dict(test=['zope.app.testing',
                                   'zope.testing',
                                   'zope.traversing',
@@ -24,10 +69,6 @@ setup(name='z3c.zrtresource',
                           'zope.schema',
                           'zope.security',
                           ],
-      zip_safe=False,
-      packages=find_packages('src'),
-      include_package_data=True,
-      package_dir = {'':'src'},
-      namespace_packages=['z3c',],
-     )
-
+      include_package_data = True,
+      zip_safe = False,
+      )
