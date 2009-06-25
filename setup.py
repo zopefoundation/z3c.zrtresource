c18150998c76088347d0e8d78f3c82106df7ff12
Michael Howitz <mh@gocept.com>
2009-06-25 17:09:04 +0000

    Back to development: 1.2.1dev

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.2.0'
+version = '1.2.1dev'
 
 
 setup(name='z3c.zrtresource',
